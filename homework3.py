d0cb5ce4c535e091542c3abc895c0afbf920eb89
OlegVasilevU <trade.vasilev@yandex.ru>
2024-06-30 15:19:27 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Динамическая типизация"
+name = 'Oleg'
+print(name)
+age = 34
+print(age)
+new_age = (age + 4)
+print(new_age)
+is_student = True
+print(is_student)
+
